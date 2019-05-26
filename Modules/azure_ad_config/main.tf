dc36d8f8015be61a68545a88318e4f3df91a68ed
Daniel Neel <dneelyep@gmail.com>
2019-05-26 18:13:04 -0400

    Start managing the public website's app registration in Terraform rather than through the portal.

--- Modules/azure_ad_config/main.tf
+++ Modules/azure_ad_config/main.tf
@@ -5,6 +5,18 @@ resource "azuread_application" "MSMApp_DBAccess" {
   available_to_other_tenants = false
 }
 
+resource "azuread_application" "MSMApp_WebsiteAppRegistration" {
+  name     = "MusicSampleManager Application: Website Service Principal"
+  homepage = "https://example.com"
+
+  reply_urls = [
+    "https://localhost:44354/",
+    "https://localhost:44354/signin-oidc",
+  ]
+
+  available_to_other_tenants = false
+}
+
 # resource "azurerm_azuread_service_principal" "MsmDbAppSPADSP" {
 #   application_id = "${azurerm_azuread_application.MsmDbAppSPADApp.application_id}"
 # }
